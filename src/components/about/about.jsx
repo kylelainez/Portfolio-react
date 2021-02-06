2e6a14980d39e99458b079f84d953905883a0210
kylelainez <kylelainez@yahoo.com.ph>
2021-02-05 20:43:53 -0800

    About Me

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './about.css'
 
 function about(){
-    return <div className="about" id="about"></div>;
+    return <div className="about" id="about">
+        <h1> About Me.</h1>
+        <div className="aboutMe">
+            Helo! I'm Kyle, a recent graduate of Software Engineering Immersive program at General Assembly.
+            I enjoy programming a lot and creating things for the computer.
+            My goal is to create programs that can facilitate people's needs.
+        </div>
+    </div>;
 }
 
 export default about;
