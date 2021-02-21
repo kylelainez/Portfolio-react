244bdb109f392f07cb61439183a14fb28874ec63
kylelainez <kyleadrianlainez@gmail.com>
2021-02-20 18:27:27 -0800

    Add list of projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -2,15 +2,58 @@ import React from 'react';
 import './projects.css';
 import Chess from '../../Images/Chess.png';
 import BuyAndSale from '../../Images/BuyAndSale.png';
-import GoalTracker from '../../Images/GoalTracer.png';
+import GoalTracker from '../../Images/GoalTracker.png';
 import Convey from '../../Images/Convey.png';
 
 function projects(){
     return <div className="projects" id="projects">
         <div className="project-list">
             <img src={Chess} />
+            <ul className="technologies">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+            </ul>
         </div>
-        
+        <div className="project-list">
+            <img src={BuyAndSale} />
+            <ul className="technologies">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>Passport.js</li>
+                <li>MongoDB</li>
+            </ul>
+        </div>
+        <div className="project-list">
+            <img src={GoalTracker} />
+            <ul className="technologies">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Python</li>
+                <li>Django</li>
+                <li>PostgreSQL</li>
+                <li>AWS S3</li>
+                <li>Bootstrap</li>
+            </ul>
+        </div>
+        <div className="project-list">
+            <img src={Convey} />
+            <ul className="technologies">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Socket.io</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>MongoDB</li>
+                <li>AWS S3</li>
+            </ul>
+        </div>
     </div>
 }
 
