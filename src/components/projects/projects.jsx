07aa1ef07b2ffaad66350d10f5266662335d4769
kylelainez <kyleadrianlainez@gmail.com>
2021-02-20 18:32:56 -0800

    Styling Projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -7,52 +7,55 @@ import Convey from '../../Images/Convey.png';
 
 function projects(){
     return <div className="projects" id="projects">
-        <div className="project-list">
-            <img src={Chess} />
-            <ul className="technologies">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-            </ul>
-        </div>
-        <div className="project-list">
-            <img src={BuyAndSale} />
-            <ul className="technologies">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>Node.js</li>
-                <li>Express</li>
-                <li>Passport.js</li>
-                <li>MongoDB</li>
-            </ul>
-        </div>
-        <div className="project-list">
-            <img src={GoalTracker} />
-            <ul className="technologies">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>Python</li>
-                <li>Django</li>
-                <li>PostgreSQL</li>
-                <li>AWS S3</li>
-                <li>Bootstrap</li>
-            </ul>
-        </div>
-        <div className="project-list">
-            <img src={Convey} />
-            <ul className="technologies">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Socket.io</li>
-                <li>Node.js</li>
-                <li>Express</li>
-                <li>MongoDB</li>
-                <li>AWS S3</li>
-            </ul>
+        <h1>Projects</h1> 
+        <div>
+            <div className="project-list">
+                <img src={Chess} />
+                <ul className="technologies">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+            </div>
+            <div className="project-list">
+                <img src={BuyAndSale} />
+                <ul className="technologies">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Node.js</li>
+                    <li>Express</li>
+                    <li>Passport.js</li>
+                    <li>MongoDB</li>
+                </ul>
+            </div>
+            <div className="project-list">
+                <img src={GoalTracker} />
+                <ul className="technologies">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Python</li>
+                    <li>Django</li>
+                    <li>PostgreSQL</li>
+                    <li>AWS S3</li>
+                    <li>Bootstrap</li>
+                </ul>
+            </div>
+            <div className="project-list">
+                <img src={Convey} />
+                <ul className="technologies">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>Socket.io</li>
+                    <li>Node.js</li>
+                    <li>Express</li>
+                    <li>MongoDB</li>
+                    <li>AWS S3</li>
+                </ul>
+            </div>
         </div>
     </div>
 }
