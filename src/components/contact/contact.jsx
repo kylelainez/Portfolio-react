bf95ac499e318df9c9411ee44de494e3b87f61ec
kylelainez <kyleadrianlainez@gmail.com>
2021-02-28 22:03:01 -0800

    Style Contacts

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,7 +3,7 @@ import './contact.css';
 
 function Contact(){
     return <div className="contact" id="contact">
-        <h1>Contact</h1>
+        <h1>Contact Me</h1>
         <p>Interested in working together?</p>
         <a href="mailto:kyleadrianlainez@gmail.com" target="_blank" rel="noreferrer">Send a message!</a>
     </div>
