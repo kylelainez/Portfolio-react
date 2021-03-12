7f0a1f663fa30c07145473e51a09916ed7a54604
kylelainez <kyleadrianlainez@gmail.com>
2021-03-11 19:02:57 -0800

    Add Left Component

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import NavBar from '../../components/navbar/navbar';
 import Container from '../../components/container/container'
+import LeftComponent from '../../components/leftComponent/leftComponent';
+import RightComponent from '../../components/rightComponent/rightComponent';
 import './App.css';
 
 function App() {
@@ -8,7 +10,9 @@ function App() {
     <div className="App">
       <NavBar />
       <div>
+        <LeftComponent />
         <Container />
+        <RightComponent />
       </div>
     </div>
   );
