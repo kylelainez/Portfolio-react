a0aacb0331de9238da3ae38cb2d2c006325e53d5
kylelainez <kyleadrianlainez@gmail.com>
2021-03-11 19:09:27 -0800

    Add Right Component

--- src/components/rightComponent/rightComponent.jsx
+++ src/components/rightComponent/rightComponent.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './rightComponent.css';
 
 function rightComponent(){
-    return <div></div>;
+    return <div className="rightComponent">
+        <a rel="noreferrer" href="#" className="rotated">
+            <div>
+                Kyle Adrian Lainez
+            </div>
+        </a>
+    </div>;
 }
 
 export default rightComponent;
