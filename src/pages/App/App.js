b081d46ccabbc8fb8b38b4f8f92c09597818d125
kylelainez <kyleadrianlainez@gmail.com>
2021-03-16 17:56:28 -0700

    Remove Side Components

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import NavBar from '../../components/navbar/navbar';
 import Container from '../../components/container/container'
-import LeftComponent from '../../components/leftComponent/leftComponent';
-import RightComponent from '../../components/rightComponent/rightComponent';
 import './App.css';
 
 function App() {
@@ -10,9 +8,7 @@ function App() {
     <div className="App">
       <NavBar />
       <div>
-        <LeftComponent />
         <Container />
-        <RightComponent />
       </div>
     </div>
   );
