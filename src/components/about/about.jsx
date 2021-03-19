22422bbfacfcee57d7b09e0af83ecb00d0f8e209
kylelainez <kyleadrianlainez@gmail.com>
2021-03-18 18:03:17 -0700

    Add Image to about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import './about.css'
+import './about.css';
+import MyImage from '../../Images/kyle.JPG';
 
 function about(){
     return <div className="about" id="about">
         <h1> About Me.</h1>
         <div className="aboutMe">
-            Helo! I'm Kyle, a recent graduate of Software Engineering Immersive program at General Assembly.
-            I enjoy programming a lot and creating things for the computer.
-            My goal is to create programs that can facilitate people's needs.
+            <p>
+                Helo! I'm Kyle, a recent graduate of Software Engineering Immersive program at General Assembly.
+                I enjoy programming a lot and creating things for the computer.
+                My goal is to create programs that can facilitate people's needs.
+            </p>
+            <img src={MyImage} alt="Kyle"/>
         </div>
     </div>;
 }
