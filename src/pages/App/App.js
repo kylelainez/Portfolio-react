cf291707ca1560e4931698eabf4456d37565fcb4
kylelainez <kyleadrianlainez@gmail.com>
2021-03-18 19:54:22 -0700

    Implement Smooth Scroll

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import NavBar from '../../components/navbar/navbar';
 import Container from '../../components/container/container'
 import './App.css';
+import smoothscroll from 'smoothscroll-polyfill';
 
 function App() {
+  smoothscroll.polyfill();
   return (
     <div className="App">
       <NavBar />
