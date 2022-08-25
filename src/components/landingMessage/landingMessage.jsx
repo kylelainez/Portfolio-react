09324c36dcb801c1a78af1ec4d4c1f4cd39b027d
kylelainez <kyleadrianlainez@gmail.com>
2022-08-24 20:14:01 -0700

    fix pdf file error

--- src/components/landingMessage/landingMessage.jsx
+++ src/components/landingMessage/landingMessage.jsx
@@ -1,24 +1,44 @@
 import React, { useEffect } from 'react';
-import './landingMessage.css'
+import './landingMessage.css';
 import Resume from '../../files/Kyle Lainez - Resume.pdf';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
-function LandingMessage(){
-
+function LandingMessage() {
     useEffect(() => {
-        Aos.init({duration: 2000});
+        Aos.init({ duration: 2000 });
     }, []);
-    
-    return <div className="landingMessage" id="landingMessage">
-        <span className="small-heading" data-aos="fade" data-aos-once data-aos-delay="50" >Hi, my name is</span>
-        <span className="big-heading" data-aos="fade" data-aos-once data-aos-delay="1000" >Kyle Adrian Lainez.</span>
-        <span className="big-heading" data-aos="fade" data-aos-once data-aos-delay="2000" >I'm a Software Engineer.</span>
-        <div data-aos="fade" data-aos-once data-aos-delay="3000">
-            <a href={Resume} target="_blank" rel="noreferrer">View my Resume</a>
+
+    return (
+        <div className='landingMessage' id='landingMessage'>
+            <span
+                className='small-heading'
+                data-aos='fade'
+                data-aos-once
+                data-aos-delay='50'>
+                Hi, my name is
+            </span>
+            <span
+                className='big-heading'
+                data-aos='fade'
+                data-aos-once
+                data-aos-delay='1000'>
+                Kyle Adrian Lainez.
+            </span>
+            <span
+                className='big-heading'
+                data-aos='fade'
+                data-aos-once
+                data-aos-delay='2000'>
+                I'm a Software Engineer.
+            </span>
+            <div data-aos='fade' data-aos-once data-aos-delay='3000'>
+                <a href={Resume} target='_blank' rel='noreferrer'>
+                    View my Resume
+                </a>
+            </div>
         </div>
-       
-    </div>
+    );
 }
 
 export default LandingMessage;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './navbar.css';
 import Resume from '../../files/Kyle Lainez - Resume.pdf';
-
-function navbar(){
-    function scroll(scrollId){
-       const element = document.querySelector(scrollId);
-       if(window.innerWidth <= 768) toggleBurger();
-       window.scrollTo({
-           top: element.offsetTop,
-           left: 0,
-           behavior: 'smooth'
-       });
+function navbar() {
+    function scroll(scrollId) {
+        const element = document.querySelector(scrollId);
+        if (window.innerWidth <= 768) toggleBurger();
+        window.scrollTo({
+            top: element.offsetTop,
+            left: 0,
+            behavior: 'smooth',
+        });
     }
 
     function toggleBurger() {
@@ -21,21 +20,28 @@ function navbar(){
     return (
         <header>
             <nav>
-                <div className="icon">
-                    <a href="/">KA</a>
+                <div className='icon'>
+                    <a href='/'>KA</a>
                 </div>
-                <ul className="links">
+                <ul className='links'>
                     <li onClick={() => scroll('#about')}>About</li>
                     <li onClick={() => scroll('#projects')}>Projects</li>
                     <li onClick={() => scroll('#contact')}>Contact</li>
-                    <li><a href={Resume} target="_blank" id="resume" rel="noreferrer">Resume</a></li>
+                    <li>
+                        <a
+                            href={Resume}
+                            target='_blank'
+                            id='resume'
+                            rel='noreferrer'>
+                            Resume
+                        </a>
+                    </li>
                 </ul>
             </nav>
             <div className='navButton'>
-                <div className="burger" onClick={toggleBurger}></div>
+                <div className='burger' onClick={toggleBurger}></div>
             </div>
         </header>
-       
-    )
+    );
 }
 export default navbar;
